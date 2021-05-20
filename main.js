5695b8a339579a2ff673c1fb574547b3a02e2e38
Sasha Minakova <80351442+Sasha1295@users.noreply.github.com>
2021-05-20 18:06:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function myFunction(x){
         scroll = offset;
         layer3.style.top = (55 + scroll/30) + '%';
     });
-    }
+   }
 
     var layer3 = document.getElementById('mountain')
     scroll = window.pageYOffset;
@@ -16,21 +16,20 @@ function myFunction(x){
         scroll = offset;
         layer3.style.top = (41 + scroll/10) + '%';
     });
-    }
-}
+    
 if (matchMedia) {
     const x = window.matchMedia("(max-width: 1050px)");
     x.addListener(myFunction);
     myFunction(x);
 }
-© 2021 GitHub, Inc.
+GitHub, Inc.
 Terms
 Privacy
 Security
 Status
 Docs
-Contact GitHub
-Pricing
+Contact 
+PricingGitHub
 API
 Training
 Blog
